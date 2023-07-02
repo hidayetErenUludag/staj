9ceec6f14b76fced5e083c4373bb3af2a341fca8
Hidayet Eren Uludağ <76475118+mightymountain@users.noreply.github.com>
2023-07-02 14:08:24 +0300

    Improvement

--- main.py
+++ main.py
@@ -10,13 +10,36 @@ status_list = eval(content)
 
 #mighty_dict = {}
 
-def dictionaryer(list):
-    for i in list:
-        x = i[0]
-        if len(i) > 1 and len(i[1]) > 0:
-            dict_name = x
-            for k in range(1, len(i)-1):
-                if len(i[k][1]) > 0:
-                    print(i[k][0])
+def dictionaryer(status_list):
+    dict_turkey = {}
 
-dictionaryer(status_list)
+    for i in status_list:
+        province = i[0]
+        switch_interfaces = {}
+
+        if len(i) > 1 and len(i[1]) > 0:
+            for k in range(1, len(i)):
+                switch_name = i[k][0]
+                interfaces = i[k][1:]
+                switch_interfaces[switch_name] = interfaces
+
+        dict_turkey[province] = switch_interfaces
+    return dict_turkey
+
+def finder():
+    for i in status_list:
+        province = i[0]
+        print(province)
+
+    print("please enter one of these cities")
+    x = str(input())
+    dict_general = dictionaryer(status_list)
+    print(dict_general.get(x).keys())
+    print("please select a switch")
+    y = str(input())
+    print(dict_general[x][y])
+    print("\n")
+    print("The full dictonary")
+    print(dict_general)
+
+finder()
